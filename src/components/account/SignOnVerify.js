d203f2d636ab32cb244f1c60cbf18633b9553101
Juhezi <juhezix@163.com>
2017-09-11 23:38:52 +0800

    [Add] history

--- src/components/account/SignOnVerify.js
+++ src/components/account/SignOnVerify.js
@@ -1,16 +1,20 @@
 import React from "react";
 import {signOnVerify} from "../../utils/Service";
 import {getUserInfo} from "../../utils/UserUtils";
-import {SIGN_ON} from "../../utils/Mapx";
-import {} from 'react-router'
+import {HOME} from "../../utils/Mapx";
+import createBrowserHistory from "history/es/createBrowserHistory";
 
 //登录验证
 class SignOnVerify extends React.Component {
 
+
     componentWillMount() {
         signOnVerify(getUserInfo(), result => {
             if (result.status !== 'ok') {   //未登录
-                // browserHistory.push(SIGN_ON);
+                console.log("Not Sign!");
+                let history = createBrowserHistory();
+                history.push(HOME);
+                //跳转到登陆界面
             }
         });
     }
